4bc4f040cdf2a4ab5b076e33f76fb82ca89959e3
swotr <roundtail@gmail.com>
2019-03-05 09:15:56 +0900

    reset

---
+++ resnet.py
@@ -0,0 +1,111 @@
+import numpy as np
+import tensorflow as tf
+
+from sn_ops import *
+
+def upscale(x):
+    return tf.image.resize_nearest_neighbor(x, 
+        [x.shape.as_list()[1]*2, x.shape.as_list()[2]*2])
+
+def upscale_bilinear(x):
+    return tf.image.resize_bilinear(x, 
+        [x.shape.as_list()[1]*2, x.shape.as_list()[2]*2])
+
+def downscale(x):
+    return tf.layers.average_pooling2d(x, 2, 2)
+
+def resnet_block(name, x, filters, kernel_size, scale):
+    ''' 
+    preactivation
+           |-BN-->ReLU-->C-->BN-->ReLU-->C-|
+      x_l----------------------------------add-->x_(l+1)
+
+    '''
+    with tf.variable_scope(name):        
+        l = x
+
+        l = tf.layers.batch_normalization(l)
+        l = tf.nn.relu(l)
+
+        if scale == 'up':
+            l = upscale(l)
+        l = tf.layers.conv2d(l, filters, kernel_size, padding='same',
+            kernel_initializer=tf.glorot_uniform_initializer(np.sqrt(2)), name='conv1')
+
+        l = tf.layers.batch_normalization(l)
+        l = tf.nn.relu(l)
+
+        l = tf.layers.conv2d(l, filters, kernel_size, padding='same',
+            kernel_initializer=tf.glorot_uniform_initializer(np.sqrt(2)), name='conv2')
+        if scale == 'down':
+            l = downscale(l)
+
+        sc = x
+
+        if scale == 'up':
+            sc = upscale(sc)
+        if scale == 'down':
+            sc = downscale(sc)
+        if sc.shape.as_list()[-1] != filters or scale == 'up' or scale == 'down':
+            sc = tf.layers.conv2d(sc, filters, 1, padding='same',
+                kernel_initializer=tf.glorot_uniform_initializer(np.sqrt(2)), name='sc_conv')
+
+    return l + sc
+
+def snresnet_block_D1(name, x, filters, kernel_size, scale, update_collection):
+    '''
+    almost same with sn_resnet_block except differnt normalization and non-linearity scheme
+    usually used for the first layer of discriminator
+    '''
+    with tf.variable_scope(name):
+        l = x        
+        l = snconv2d('sn_conv1', l, filters, kernel_size, 1,
+            seed=np.sqrt(2), update_collection=update_collection)
+        l = tf.nn.relu(l)
+        l = snconv2d('sn_conv2', l, filters, kernel_size, 1, 
+            seed=np.sqrt(2), update_collection=update_collection)
+        if scale == 'down':
+            l = downscale(l)        
+
+        sc = x
+        if scale == 'down':
+            sc = downscale(sc)
+        if sc.shape.as_list()[-1] != filters or scale == 'down':
+            sc = snconv2d('sn_conv_sc', sc, filters, 1, 1, 
+                seed=np.sqrt(2), update_collection=update_collection)
+        
+    return l + sc
+
+def snresnet_block(name, x, filters, kernel_size, scale, update_collection):
+    ''' 
+    w/ spectral normalization    
+    preactivation   
+           |-BN-->ReLU-->C-->BN-->ReLU-->C-|
+      x_l----------------------------------add-->x_(l+1)
+
+    '''
+    with tf.variable_scope(name):
+        l = x
+        #l = tf.layers.batch_normalization(l)            
+        l = tf.nn.relu(l)
+        if scale == 'up':
+            l = upscale(l)
+        l = snconv2d('sn_conv1', l, filters, kernel_size, 1, 
+            seed=np.sqrt(2), update_collection=update_collection)
+        #l = tf.layers.batch_normalization(l)
+        l = tf.nn.relu(l)
+        l = snconv2d('sn_conv2', l, filters, kernel_size, 1, 
+            seed=np.sqrt(2), update_collection=update_collection)
+        if scale == 'down':
+            l = downscale(l)
+
+        sc = x
+        if scale == 'up':
+            sc = upscale(sc)
+        elif scale == 'down':
+            sc = downscale(sc)
+        if sc.shape.as_list()[-1] != filters or scale == 'up' or scale == 'down':
+            sc = snconv2d('sn_conv_sc', sc, filters, 1, 1, 
+                seed=np.sqrt(2), update_collection=update_collection)
+
+    return l + sc
---
+++ snops.py
@@ -0,0 +1,74 @@
+import numpy as np
+import tensorflow as tf
+
+def spectral_normed_weight(w, num_iters=1, update_collection=None):
+    """Performs Spectral Normalization on a weight tensor.
+
+    Specifically it divides the weight tensor by its largest singular value. This
+    is intended to stabilize GAN training, by making the discriminator satisfy a
+    local 1-Lipschitz constraint.
+    Based on [Spectral Normalization for Generative Adversarial Networks][sn-gan]
+    [sn-gan] https://openreview.net/pdf?id=B1QRgziT-
+
+    Args:
+    w: The weight tensor which requires spectral normalization
+    num_iters: Number of SN iterations.
+    update_collection: The update collection for assigning persisted variable u.
+                        If None, the function will update u during the forward
+                        pass. Else if the update_collection equals 'NO_OPS', the
+                        function will not update the u during the forward. This
+                        is useful for the discriminator, since it does not update
+                        u in the second pass.
+                        Else, it will put the assignment in a collection
+                        defined by the user. Then the user need to run the
+                        assignment explicitly.
+    """    
+    w_shape = w.shape.as_list()
+    w = tf.reshape(w, [-1, w_shape[-1]])  # [-1, output_channel]
+    u = tf.get_variable('u', [1, w_shape[-1]],
+                        initializer=tf.truncated_normal_initializer(), trainable=False)
+    _u = u
+    _v = None
+    for _ in range(num_iters):
+        _v = tf.nn.l2_normalize(tf.matmul(_u, w, transpose_b=True))
+        _u = tf.nn.l2_normalize(tf.matmul(_v, w))
+    
+    sigma = tf.squeeze(tf.matmul(tf.matmul(_v, w), _u, transpose_b=True))
+    tf.summary.scalar('sigma', sigma) # DEBUG
+    w /= sigma
+    if update_collection is None:
+        with tf.control_dependencies([u.assign(_u)]):
+            w = tf.reshape(w, w_shape)
+    else:
+        w = tf.reshape(w, w_shape)
+        if update_collection != 'NO_OPS':
+            tf.add_to_collection(update_collection, u.assign(_u))
+    return w
+
+def snconv2d(name, input, output_dim, kernel_size, stride, seed=None, update_collection=None):
+    ''' spectral normed convolution '''
+    with tf.variable_scope(name):
+        w = tf.get_variable('W', [kernel_size, kernel_size, input.get_shape()[-1], output_dim], tf.float32, 
+            initializer=tf.glorot_uniform_initializer(seed), trainable=True)        
+        w = spectral_normed_weight(w, update_collection=update_collection)
+        conv = tf.nn.conv2d(input, w, strides=[1, stride, stride, 1], padding='SAME')
+        biases = tf.get_variable('b', [output_dim], 
+            initializer=tf.constant_initializer(0.0), trainable=True)
+        conv = tf.nn.bias_add(conv, biases)
+
+        return conv
+
+def snlinear(name, input, output_size, seed=None, use_bias=True, update_collection=None):
+    ''' spectral normed linear '''
+    shape = input.shape.as_list()
+    with tf.variable_scope(name):
+        w = tf.get_variable('W', [shape[1], output_size], tf.float32,
+            initializer=tf.glorot_uniform_initializer(seed), trainable=True)        
+        w = spectral_normed_weight(w, update_collection=update_collection)
+        mul = tf.matmul(input, w)
+        if use_bias:
+            bias = tf.get_variable('b', [output_size], 
+                initializer=tf.constant_initializer(0.0), trainable=True)
+            mul = tf.nn.bias_add(mul, bias)
+
+        return mul
